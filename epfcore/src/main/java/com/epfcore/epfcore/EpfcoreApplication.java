53bfbd14b984bcfb2e44aa86c98cb5592cc685fb
TristanBrouille <tristan.dhersin@gmail.com>
2026-04-01 14:26:55 +0200

    add project

---
+++ epfcore/src/main/java/com/epfcore/epfcore/EpfcoreApplication.java
@@ -0,0 +1,13 @@
+package com.epfcore.epfcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpfcoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpfcoreApplication.class, args);
+	}
+
+}
---
+++ epfcore/src/test/java/com/epfcore/epfcore/EpfcoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epfcore.epfcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpfcoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
