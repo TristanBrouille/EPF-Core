2246fc0fa9da0de43034e5163e6180afb1a8b610
TristanBrouille <tristan.dhersin@gmail.com>
2026-04-07 18:10:26 +0200

    ajout me rework login

--- epfcore_frontend/src/app/dashboard/dashboard.ts
+++ epfcore_frontend/src/app/dashboard/dashboard.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import {Router} from '@angular/router';
+import {loginService} from '../login/loginService';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,5 +10,11 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
+
+  constructor(protected router : Router, private fb: FormBuilder, private authService: loginService) {}
+
+  async test(){
+      await this.authService.me();
+  }
 
 }
