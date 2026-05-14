46525e0a158ce800ef4b297e14ad475a1eaf32f0
ClaRave28 <clarisse15032004@gmail.com>
2026-05-14 19:18:07 +0200

    fiche pdf complète changements

--- epfcore/src/main/java/com/epfcore/epfcore/student/service/GenerationPdfService.java
+++ epfcore/src/main/java/com/epfcore/epfcore/student/service/GenerationPdfService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import com.epfcore.epfcore.student.entity.Student;
 
 import java.io.ByteArrayOutputStream;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class GenerationPdfService {
@@ -33,10 +35,23 @@ public class GenerationPdfService {
     String nom = student.getUser().getLastname();
     String prenom = student.getUser().getFirstname();
     String email = student.getUser().getEmail();
+    LocalDate birthDate = student.getUser().getBirthDate();
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    String birthDateFormatee = birthDate.format(formatter);
+    String nationality = student.getNationality();
     String phone = student.getPhone();
+    String numeroFormate = phone.replaceAll("(.{2})", "$1 ").trim();
     String numero = student.getStudentNumber();
     String major = student.getMajor();
-    String year = student.getAcademicYear();
+    String address = student.getAddress();
+    String lastDegree = student.getLastDegree();
+    LocalDate enrollmentDate = student.getEnrollmentDate();
+    DateTimeFormatter formatter1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+    String EnrollmentDateFormatee = enrollmentDate.format(formatter1);
+    Boolean bourse = student.getScholarship();
+    String bourseFormatee = Boolean.TRUE.equals(bourse) ? "Boursier" : "Non boursier";
+    String campus = student.getCampus();
+    String academicYear= student.getAcademicYear();
 
     return """
     <html>
@@ -44,9 +59,9 @@ public class GenerationPdfService {
         <style>
             body { font-family: Arial; padding: 20px; }
 
-            .card { border: 1px solid #ddd; padding: 15px; margin-bottom: 15px; border-radius: 10px; }
+            .card { border: 1px solid #ddd; padding: 15px; margin-bottom: 30px; border-radius: 10px; }
 
-            .title { text-align: center; font-size: 22px; font-weight: bold; }
+            .title { text-align: center; font-size: 22px; font-weight: bold;margin-bottom: 40px; }
 
             .grid { display: grid; grid-template-columns: 1fr 1fr; gap: 10px; }
 
@@ -59,24 +74,38 @@ public class GenerationPdfService {
         <div class="title">FICHE ÉTUDIANT</div>
 
         <div class="card">
-            <h3>Informations</h3>
             <div class="grid">
-                <div><span class="label">Nom:</span> %s</div>
-                <div><span class="label">Prénom:</span> %s</div>
-                <div><span class="label">Email:</span> %s</div>
-                <div><span class="label">Téléphone:</span> %s</div>
+                 <p><b>Bourse:</b> %s</p>
+                 <p><b>Campus:</b> %s</p>
+                 <p><b>Année:</b> %s</p>
+                
             </div>
         </div>
 
         <div class="card">
-            <h3>Scolarité</h3>
-            <p><b>Numéro:</b> %s</p>
+            <h2>Informations</h2>
+            <div class="grid">
+                 <p><b>Nom:</b> %s</p>
+                 <p><b>Prénom:</b> %s</p>
+                 <p><b>Email:</b> %s</p>
+                 <p><b>Nationalité:</b> %s</p>
+                 <p><b>Date d'anniversaire: </b> %s</p>
+                 <p><b>Téléphone:</b> %s</p>
+                 <p><b>Adresse:</b> %s</p>
+                
+            </div>
+        </div>
+
+        <div class="card">
+            <h2>Scolarité</h2>
+            <p><b>Numéro étudiant:</b> %s</p>
             <p><b>Filière:</b> %s</p>
-            <p><b>Année:</b> %s</p>
+            <p><b>Dernier diplôme:</b> %s</p>
+            <p><b>Date inscription:</b> %s</p>
         </div>
 
     </body>
     </html>
-    """.formatted(nom, prenom, email, phone, numero, major, year);
+    """.formatted(bourseFormatee, campus, academicYear, nom, prenom, email, nationality,birthDateFormatee,numeroFormate, address,  numero, major, lastDegree, EnrollmentDateFormatee);
 }
 }
