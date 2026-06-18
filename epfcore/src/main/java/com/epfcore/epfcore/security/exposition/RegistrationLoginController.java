af3b81a96249efa3fa0a2e5c8fd512c08b52b9b4
TristanBrouille <tristan.dhersin@gmail.com>
2026-06-18 18:02:07 +0200

    #46 : Enregistrement et modification des utilisateur par admin

--- epfcore/src/main/java/com/epfcore/epfcore/security/exposition/RegistrationLoginController.java
+++ epfcore/src/main/java/com/epfcore/epfcore/security/exposition/RegistrationLoginController.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,7 +25,7 @@ public class RegistrationLoginController {
     public RegistrationLoginController(AuthenticationService authService) {
         this.authService = authService;
     }
-
+    @PreAuthorize("hasAuthority('ADMIN')")
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@RequestBody User user) {
         try {
